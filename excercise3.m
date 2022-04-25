f97a5e4183baa7e5f47280006b64683004545ab0
MANDAR PATIL <33731455+mandarmp@users.noreply.github.com>
2022-04-26 00:18:20 +0100

    creation of node output plot
    running history and GUI involved

--- excercise3.m
+++ excercise3.m
@@ -27,37 +27,90 @@ i=1;
 h =-5;
 coeffC = -1;
 coeffD = -0.5;
-targetInfo1 = 1;
+targetInfo1 = 2;
 targetInfo2 = 0;
 targetInfo3 = 0;
-levelInfo1 = 2.5;
-levelInfo2 = 5; 
-levelInfo3 = 7;
+levelInfo1 = 2.5 ;
+levelInfo2 = 5 ; 
+levelInfo3 = 7 ;
 %%
 sim = Simulator();
 
-sim.addElement(InputLoader('targetImage',targetInfo1, targetInfo2, targetInfo3, levelInfo1, levelInfo2, levelInfo3));
+sim.addElement(InputLoader('extractImage',targetInfo1, targetInfo2, targetInfo3, levelInfo1, levelInfo2, levelInfo3));
 %InputLoader(label, targetInfo1, targetInfo2, targetInfo3, levelInfo1, levelInfo2, levelInfo3)
-sim.addElement(SaliencyCompetitionNode('node', 10, h, 4, 1, 0, samplingRange, samplingResolution,coeffC,coeffD))
+sim.addElement(SaliencyCompetitionNode('node', 10, h, 4, 1, 0, samplingRange, samplingResolution))
 % TwoColourNode(label, tau, h, beta, selfExcitation, range, resolution)
-sim.addConnection('targetImage','targetActivation','node');
-sim.addConnection('targetImage','distractorSaliencyActivation','node');
-sim.init()
-sim.run(2)
-%colorMap = parula(5);
-
-
-for x = 1 : 20
-    sim.step()
-    y(x,:) = sim.getComponent('node', 'output');
-end
-figure;
-subplot(1,2,1)
-hold on;
-plot(1:20,y(:,1)')
-plot(1:20,y(:,2)')
-plot(1:20,y(:,3)')
+sim.addConnection('extractImage','targetActivation','node');
+sim.addConnection('extractImage','distractorSaliencyActivation','node');
+sim.addElement(RunningHistory('history u_1', [1, 1], historyDuration, 1), 'node', 'loc1');
+sim.addElement(RunningHistory('history u_2', [1, 1], historyDuration, 1), 'node', 'loc2');
+sim.addElement(RunningHistory('history u_3', [1, 1], historyDuration, 1), 'node', 'loc3');
+% sim.init()
+% sim.run(2)
+% %colorMap = parula(5);
+% 
+% 
+% for x = 1 : 20
+%     sim.step()
+%     y(x,:) = sim.getComponent('node', 'output');
+% end
+% figure;
+% subplot(1,2,1)
+% hold on;
+% plot(1:20,y(:,1)')
+% plot(1:20,y(:,2)')
+% plot(1:20,y(:,3)')
 
 sim.addElement(NeuralField('field v', fieldSize, 10, -5, 4));
 %hopefully this will show the 
 
+elementGroups = {'node'} ;
+
+% create the gui object
+gui = StandardGUI(sim, [50, 50, 700, 500], 0.05, ...
+  [0.0, 1/3, 1.0, 2/3], [1, 1], 0.1, ...
+  [0.0, 0.0, 1.0, 1/3], [6, 3], elementGroups, elementGroups);
+
+
+% % add a plot of nodes (with activation, output)
+gui.addVisualization(MultiPlot({'history u_1', 'history u_2','history u_3'}, ...
+  {'output', 'output','output'}, [1, 1, 1], 'horizontal', ...
+ {'XLim', [-historyDuration, 10], 'YLim', [-1, 1], 'Box', 'on', 'XGrid', 'on', 'YGrid', 'on'}, ...
+  { {'r-','LineWidth', 2, 'XData', 0:-1:-historyDuration+1,}, {'Color', [0, 0.5, 0], 'LineWidth', 2, 'XData', 0:-1:-historyDuration+1}, ...
+  {'b-', 'LineWidth', 2, 'XData', 0:-1:-historyDuration+1} }, ...
+  'node', 'relative time', 'output'), [1, 1]);
+% gui.addVisualization(ScaledImage('node', 'activation', [-10, 10], {}, {}, 'field u activation'), [1, 1]);
+% gui.addVisualization(ScaledImage('node', 'output', [0, 1], {}, {}, 'field u output'), [1, 2]);
+
+% resting level of node
+gui.addControl(ParameterSlider('h', 'node', 'h', [-10, 10],...
+  '%0.1f', 1, 'resting level of field u'), [2, 1]);
+
+
+% stimulus settings
+gui.addControl(ParameterSlider('t1', 'extractImage', 'targetInfo1', [0, 5], '%0.1f', 1, ...
+  'target info 1'), [3, 1]);
+gui.addControl(ParameterSlider('t2', 'extractImage', 'targetInfo2', [0, 5], '%0.1f', 1, ...
+  'target info 2'), [3, 2]);
+
+gui.addControl(ParameterSlider('t3', 'extractImage', 'targetInfo3', [0, 5], '%0.1f', 1, ...
+  'target info 3'), [4,1 ]);
+gui.addControl(ParameterSlider('d1', 'extractImage', 'levelInfo1', [0, 10], '%0.1f', 1, ...
+  'level info 1'), [5, 1]);
+gui.addControl(ParameterSlider('d2', 'extractImage', 'levelInfo2', [0, 10], '%0.1f', 1, ...
+  'level info 2'), [5, 2]);
+
+gui.addControl(ParameterSlider('d3', 'extractImage', 'levelInfo3', [0, 10], '%0.1f', 1, ...
+  'level info 3'), [6,1 ]);
+
+gui.addControl(GlobalControlButton('Pause', gui, 'pauseSimulation', true, false, false, 'pause simulation'), [1, 3]);
+gui.addControl(GlobalControlButton('Reset', gui, 'resetSimulation', true, false, true, 'reset simulation'), [2, 3]);
+gui.addControl(GlobalControlButton('Parameters', gui, 'paramPanelRequest', true, false, false, 'open parameter panel'), [3, 3]);
+gui.addControl(GlobalControlButton('Save', gui, 'saveParameters', true, false, true, 'save parameter settings'), [4, 3]);
+gui.addControl(GlobalControlButton('Load', gui, 'loadParameters', true, false, true, 'load parameter settings'), [5, 3]);
+gui.addControl(GlobalControlButton('Quit', gui, 'quitSimulation', true, false, false, 'quit simulation'), [6, 3]);
+
+
+% run the simulation in the GUI
+sim.init()
+gui.run(inf);
