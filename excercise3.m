73f42bf0acf4f14a34204858d48a515d85d9c8df
MANDAR PATIL <33731455+mandarmp@users.noreply.github.com>
2022-05-29 21:21:31 +0100

    Complete pipeline with threshold for IL set up

--- excercise3.m
+++ excercise3.m
@@ -5,91 +5,85 @@ close all;
 clc;
 
 
-fieldSize=[30, 30];
-currentSelection = 1;
 
-%% 
-connectionValue = -2; %Amplitude for connections can be different for each one
+%% Parameter initialisation
+
+fieldSize=[30, 30];
 historyDuration = 100;
 samplingRange = [-10, 10];
 samplingResolution = 0.05;
-tStimOn=0;
-
-sigmaInhY = 10;
-sigmaInhX = 10;
-amplitudeInh_dv = 0.8;
-
-sigmaExcY = 5;
-sigmaExcX = 5;
-amplitudeExc = 5;
-
-amplitudeGlobal_dd=-0.01;
-amplitudeGlobal_dv=-0.005;
-i=1;
-h =3;
-selfexc = 1;
-beta = 4;
-coeffC = -1;
-coeffD = -1.0;
-
-levelInfo1 = 3;
-levelInfo2 = 6.0 ; 
-levelInfo3 = 6.0 ;
-
-
 sigma_exc = 1;
-%%
+
+%% settings for lta
+% h =3;
+% selfexc = 1;
+% beta = 1;
+% coeffA = 1;
+% coeffC = -1;
+% coeffD = -2;
+% levelInfo1 = 1.5;
+% levelInfo2 = 6; 
+% levelInfo3 = 8;
+
+%% settings for Wta
+h =-1.5;
+selfexc = 5;
+beta = 1;
+coeffA = 1;
+coeffC = 0.7;
+coeffD = -4.0;
+levelInfo1 = 5;
+levelInfo2 = 4.6; 
+levelInfo3 = 4.8;
+
+
+%% Setting up Cosivinia elements
 sim = Simulator();
 
-sim.addElement(InputLoader('extractImage',levelInfo1, levelInfo2, levelInfo3));
-%InputLoader(label, targetInfo1, targetInfo2, targetInfo3, levelInfo1, levelInfo2, levelInfo3)
-sim.addElement(SaliencyCompetitionNode('node', 100, h, beta, selfexc, 0, samplingRange, samplingResolution,coeffC,coeffD))
-% TwoColourNode(label, tau, h, beta, selfExcitation, range, resolution)
-% sim.addConnection('extractImage','targetActivation','node');
+%loading the inputs into the cosivinia framework (done by InputLoader
+% later this will be modified into ImageProcessing component)
+%Argument list for InputLoader is (label, targetInfo1, targetInfo2, targetInfo3,
+%levelInfo1, levelInfo2, levelInfo3)
+sim.addElement(InputLoader('extractImage',levelInfo1, levelInfo2, levelInfo3)); 
+
+%Argument list for SaliencyCompetitionNode is (label, tau, h, beta, 
+%selfExcitation, range, resolution)
+sim.addElement(SaliencyCompetitionNode('node', 10, h, beta, selfexc, 0, samplingRange, samplingResolution,coeffA ,coeffC,coeffD))
 sim.addConnection('extractImage','SaliencyActivation','node');
+%history for visualisation purpose.
 sim.addElement(RunningHistory('history u_1', [1, 1], historyDuration, 1), 'node', 'loc1');
 sim.addElement(RunningHistory('history u_2', [1, 1], historyDuration, 1), 'node', 'loc2');
 sim.addElement(RunningHistory('history u_3', [1, 1], historyDuration, 1), 'node', 'loc3');
 
+%Using Gaussian stimulus to create the spatial location maps of the items.
 sim.addElement(ModifiedGaussStimulus2D('item1', fieldSize, sigma_exc, sigma_exc,5 ,8,8));
 sim.addElement(ModifiedGaussStimulus2D('item2', fieldSize, sigma_exc, sigma_exc, 5, 4,15 ));
 sim.addElement(ModifiedGaussStimulus2D('item3', fieldSize, sigma_exc,  sigma_exc, 5,8,23));
+
+%This component will do the weighted sum of the Gaussian maps with the node
+%output.
 sim.addElement(ModifiedPointwiseProduct('pointwiseNode',fieldSize));
 sim.addConnection('node','output','pointwiseNode');
 sim.addConnection('item1','output','pointwiseNode');
 sim.addConnection('item2','output','pointwiseNode');
 sim.addConnection('item3','output','pointwiseNode');
 
-sim.addElement(NeuralField('targetLocationMap', fieldSize, 20, -1, 4)); % NeuralField(label, size, tau, h, beta)
+%Creation of the Target Location map.
+% Arguments to the NeuralField are (label, size, tau, h, beta)
+sim.addElement(ModifiedNeuralField('targetLocationMap', fieldSize, 20, -1.5, 2)); 
 sim.addConnection('pointwiseNode','output','targetLocationMap');
+%Running history for visulisation process.
+sim.addElement(RunningHistory('history u_4', [1, 1], historyDuration, 1), 'targetLocationMap', 'loc1');
+sim.addElement(RunningHistory('history u_5', [1, 1], historyDuration, 1), 'targetLocationMap', 'loc2');
+sim.addElement(RunningHistory('history u_6', [1, 1], historyDuration, 1), 'targetLocationMap', 'loc3');
 
 
-
-% sim.init()
-% sim.run(2)
-% %colorMap = parula(5);
-% 
-% 
-% for x = 1 : 20
-%     sim.step()
-%     y(x,:) = sim.getComponent('node', 'output');
-% end
-% figure;
-% subplot(1,2,1)
-% hold on;
-% plot(1:20,y(:,1)')
-% plot(1:20,y(:,2)')
-% plot(1:20,y(:,3)')
-
-sim.addElement(NeuralField('field v', fieldSize, 10, -5, 4));
-%hopefully this will show the 
+%% GUI 
 
 elementGroups = {'node'} ;
 
-% % create the gui object
-% gui = StandardGUI(sim, [50, 50, 700, 500], 0.05, ...
-%   [0.0, 1/3, 1.0, 2/3], [1, 1], 0.1, ...
-%   [0.0, 0.0, 1.0, 1/3], [8, 4], elementGroups, elementGroups);
+% create the gui object
+
 gui = StandardGUI(sim,  [50, 50, 720, 400], 0, [0.0, 0.0, 0.75, 1.0], [3, 2], [0.03, 0.05], ...
     [0.0, 0.0, 1.0, 1/3], [8, 4], elementGroups, elementGroups);
 
@@ -99,17 +93,30 @@ gui.addVisualization(MultiPlot({'history u_1', 'history u_2','history u_3'}, ...
  {'XLim', [-historyDuration, 10], 'YLim', [-0.2, 1], 'Box', 'on', 'XGrid', 'on', 'YGrid', 'on'}, ...
   { {'r-','LineWidth', 2, 'XData', 0:-1:-historyDuration+1,}, {'Color', [0, 0.5, 0], 'LineWidth', 2, 'XData', 0:-1:-historyDuration+1}, ...
   {'b-', 'LineWidth', 2, 'XData', 0:-1:-historyDuration+1} }, ...
-  'node', 'relative time', 'output'), [1, 1]);
-% gui.addVisualization(ScaledImage('node', 'activation', [-10, 10], {}, {}, 'field u activation'), [1, 1]);
-% gui.addVisualization(ScaledImage('node', 'output', [0, 1], {}, {}, 'field u output'), [1, 2]);
-gui.addVisualization(ScaledImage('targetLocationMap', 'output',[0, 1], {'FontSize',5,'XTick', [], 'YTick', []},{}, '\fontsize{5}Target Location Map Output'), [1, 2],[1, 1]);
+  'Saliency Node', 'relative time', 'output'), [1, 1],[1,1]);
+
+gui.addVisualization(ScaledImage('targetLocationMap', 'output',[0, 1], {'FontSize',5,'XTick', [], 'YTick', []},{}, '\fontsize{5}Target Location Map Output'), [1, 3],[1, 0.75]);
+
+gui.addVisualization(MultiPlot({'history u_4', 'history u_5','history u_6'}, ...
+  {'output', 'output','output'}, [1, 1, 1], 'horizontal', ...
+ {'XLim', [-historyDuration, 10], 'YLim', [-0.2, 1], 'Box', 'on', 'XGrid', 'on', 'YGrid', 'on'}, ...
+  { {'r-','LineWidth', 2, 'XData', 0:-1:-historyDuration+1,}, {'Color', [0, 0.5, 0], 'LineWidth', 2, 'XData', 0:-1:-historyDuration+1}, ...
+  {'b-', 'LineWidth', 2, 'XData', 0:-1:-historyDuration+1} }, ...
+  'Target location Map', 'relative time', 'output'), [1, 2]);
+
 % resting level of node
+gui.addControl(ParameterSlider('h', 'targetLocationMap', 'h', [-10, 10],...
+  '%0.1f', 1, 'resting level of node'), [2, 1]);
+gui.addControl(ParameterSlider('beta', 'targetLocationMap', 'beta', [-10, 10],...
+  '%0.1f', 1, 'resting level of node'), [2, 3]);
 gui.addControl(ParameterSlider('h', 'node', 'h', [-10, 10],...
   '%0.1f', 1, 'resting level of node'), [3, 1]);
 gui.addControl(ParameterSlider('selfExc', 'node', 'selfExcitation', [-10, 10],...
   '%0.1f', 1, 'resting level of node'), [3, 2]);
 gui.addControl(ParameterSlider('beta', 'node', 'beta', [-10, 10],...
   '%0.1f', 1, 'resting level of node'), [3, 3]);
+gui.addControl(ParameterSlider('coeffA', 'node', 'coeffA', [-5, 5],...
+  '%0.1f', 1, 'resting level of node'), [5, 1]);
 gui.addControl(ParameterSlider('coeffC', 'node', 'coeffC', [-5, 5],...
   '%0.1f', 1, 'resting level of node'), [5, 2]);
 gui.addControl(ParameterSlider('coeffD', 'node', 'coeffD', [-5, 5],...
@@ -131,6 +138,7 @@ gui.addControl(GlobalControlButton('Save', gui, 'saveParameters', true, false, t
 gui.addControl(GlobalControlButton('Load', gui, 'loadParameters', true, false, true, 'load parameter settings'), [5, 4]);
 gui.addControl(GlobalControlButton('Quit', gui, 'quitSimulation', true, false, false, 'quit simulation'), [6, 4]);
 
+%% GUI operation.
 
 % maximal simulation time
 tMax = inf;
@@ -140,24 +148,42 @@ sim.init();
 gui.init();
 
 % run and visualize simulation manually step by step
+
 exp = 0;
+timing = 0;
+activation = 0
+ts =0;
 %time = zeros(1,500);
 while ~gui.quitSimulation && sim.t < tMax
+  if gui.resetSimulation
+      ts = 0;
+      clear timing activation
+  end
   gui.step();
-  %numberOf
   
 %   if any(sim.getComponent('field u', 'activation') > 5)
 %     sim.setElementParameters('stim A', 'amplitude', 0);
-%     sim.setElementParameters('stim B', 'amplitude', 0);
+%     sim.setElementParameters('stim B', 'amplitude', 0);     %test codes..
 %     gui.checkAndUpdateControls();
 %   end
-% 
+
   targetOutput = sim.getComponent('targetLocationMap','output');
   gui.checkAndUpdateControls();
-  time(1,sim.t) = targetOutput(4,15);
-  if sim.t == 500
+  %2D activation map - op at centroid of the items being analysed.
+  activation(1,sim.t) = targetOutput(8,8);   
+  activation(2,sim.t) = targetOutput(4,15);
+  activation(3,sim.t) = targetOutput(8,23);
+  
+  % finding the threshold and registering time ( initiation latency)
+  if any(targetOutput(:)>0.5)
+     ts = ts + 1;
+     timing(1,ts) = sim.t;
+  end
+  if sim.t == 300
     exp = exp + 1;
-    timingdata(exp,:) = time(1,1:500);
+    experimentData{exp,1} = activation(:,1:300);
+    experimentData{exp,2} = timing(1,1)
+
     disp('assigned')
   end
     
--- plotforexcercise3.m
+++ plotforexcercise3.m
@@ -1,8 +1,48 @@
 
 clear all
-timing(1,:) = getfield(load('exp1.mat','timingdata'),'timingdata');
-timing(2,:) = getfield(load('exp2.mat','timingdata'),'timingdata');
-timing(3,:) = getfield(load('exp3.mat','timingdata'),'timingdata');
-timing(4,:) = getfield(load('exp4.mat','timingdata'),'timingdata');
-timing(5,:) = getfield(load('exp5.mat','timingdata'),'timingdata');
-timing(6,:) = getfield(load('exp6.mat','timingdata'),'timingdata');
+close all 
+clc
+clf
+
+% timing1 = getfield(load('exp1.mat','timingdata'),'timingdata');
+% timing2 = getfield(load('exp2.mat','timingdata'),'timingdata');
+% timing3 = getfield(load('exp3.mat','timingdata'),'timingdata');
+% timing4 = getfield(load('exp4.mat','timingdata'),'timingdata');
+% timing5 = getfield(load('exp5.mat','timingdata'),'timingdata');
+% timing6 = getfield(load('exp6.mat','timingdata'),'timingdata');
+
+timing1 = getfield(load('lta.mat','timing1'),'timing1');
+timing2 = getfield(load('lta.mat','timing2'),'timing2');
+timing3 = getfield(load('lta.mat','timing3'),'timing3');
+timing4 = getfield(load('lta.mat','timing4'),'timing4');
+timing5 = getfield(load('lta.mat','timing5'),'timing5');
+timing6 = getfield(load('lta.mat','timing6'),'timing6');
+% timing(7,:) = getfield(load('exp11.mat','timingdata'),'timingdata');
+% timing(8,:) = getfield(load('exp12.mat','timingdata'),'timingdata');
+% timing(9,:) = getfield(load('exp9.mat','timingdata'),'timingdata');
+subplot(2,3,1)
+plot(timing1','LineWidth',1.05)
+ylim([-0.2 1.2])
+title('3 6 8 TDd')
+subplot(2,3,2)
+plot(timing2','LineWidth',1.05)
+ylim([-0.2 1.2])
+title('3 6 6 Tdd')
+subplot(2,3,3)
+plot(timing3','LineWidth',1.05)
+ylim([-0.2 1.2])
+title('3 8 8 TDD')
+subplot(2,3,4)
+plot(timing4','LineWidth',1.05)
+ylim([-0.2 1.2])
+title('3 8 12 tDd')
+subplot(2,3,5)
+plot(timing5','LineWidth',1.05)
+ylim([-0.2 1.2])
+title('3 8 8 tdd')
+subplot(2,3,6)
+plot(timing6','LineWidth',1.05)
+ylim([-0.2 1.2])
+title('3 12 12 tDD')
+legend('loc1','loc2','loc3')
+% legend({'3 6 8 TDd','3 6 6 Tdd','3 8 8 TDD','3 8 12 tDd','3 10 10 tdd','3 12 12 tDD'})
